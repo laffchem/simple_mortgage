a80879756bb513566222db061792a089a52891f8
John Laffey <laffeychem@gmail.com>
2024-12-01 22:33:27 -0500

    add dockerfile

--- simplemortgage/simplemortgage/settings.py
+++ simplemortgage/simplemortgage/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = "django-insecure-0qozwvl!-29res@_c#b532%qge*_0$a0+gdrk8s@mgxr%y72_g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -131,8 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
 
 # Media Files
 MEDIA_URL = "/media/"
